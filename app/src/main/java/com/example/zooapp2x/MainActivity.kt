ddd4091ab60dd7f29f863ec0321f32b23fdbbdaf
Andre Lussier <Dremon7@hotmail.com>
2019-08-22 23:13:37 -0400

    initial commit

---
+++ app/src/main/java/com/example/zooapp2x/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.zooapp2x
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
